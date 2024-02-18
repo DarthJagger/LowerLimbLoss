02f2c6f49e98bdbbf1fc3a7274ebb38c0f0be4be
Zachnew01 <zacharytnewman@msn.com>
2024-02-18 17:46:33 -0500

    Site Admin stuff for eventual user creation

--- Main_Aplication/admin.py
+++ Main_Aplication/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Patients, Providers, TimePoints, Authorizations, PatientEntries, SensorEntries, PlusMScores, AmpproScores, AmpnoproScores, TimedupandgoScores, SixminwalktestScores
 
 # Register your models here.
+admin.site.register(Patients)
+admin.site.register(Providers)
+admin.site.register(TimePoints)
+admin.site.register(Authorizations)
+admin.site.register(PatientEntries)
+admin.site.register(SensorEntries)
+admin.site.register(PlusMScores)
+admin.site.register(AmpproScores)
+admin.site.register(AmpnoproScores)
+admin.site.register(TimedupandgoScores)
+admin.site.register(SixminwalktestScores)
