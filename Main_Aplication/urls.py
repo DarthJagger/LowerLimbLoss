92f3885bbf34fc4930ec140d73341b5bd9bf1990
Jagger <jaggergilleland@gmail.com>
2024-03-25 13:05:44 -0400

    Updated header for all pages and removed deprecated unsignedin views

--- Main_Aplication/urls.py
+++ Main_Aplication/urls.py
@@ -14,11 +14,6 @@ urlpatterns = [
     path("Provider", views.Provider, name="Navigation"),
     path("Enter_Scores", views.Enter_scores, name="Navigation"),
 
-    path("Postsurgical_Stabilization", views.Postsurgical_Stabilization, name="Navigation"),
-    path("Preprosthetic_Rehabilitation", views.Preprosthetic_Rehabilitation, name="Navigation"),
-    path("Limb_Healing", views.Limb_Healing, name="Navigation"),
-    path("Prosthetic_Fitting", views.Prosthetic_Fitting, name="Navigation"),
-    path("Prosthetic_Rehabilitation", views.Prosthetic_Rehabilitation, name="Navigation"),
     path("Patient_Postsurgical_Stabilization", views.Patient_Postsurgical_Stabilization, name="Navigation"),
     path("Patient_Preprosthetic_Rehabilitation", views.Patient_Preprosthetic_Rehabilitation, name="Navigation"),
     path("Patient_Limb_Healing", views.Patient_Limb_Healing, name="Navigation"),
--- Main_Aplication/views.py
+++ Main_Aplication/views.py
@@ -131,24 +131,6 @@ def Enter_scores(request):
     return render(request, "Patient_enter_scores.html")
 
 
-def Postsurgical_Stabilization(request):
-    return render(request, "Postsurgical_Stabilization.html")
-
-
-def Preprosthetic_Rehabilitation(request):
-    return render(request, "Preprosthetic_Rehabilitation.html")
-
-
-def Limb_Healing(request):
-    return render(request, "Limb_Healing.html")
-
-
-def Prosthetic_Fitting(request):
-    return render(request, "Prosthetic_Fitting.html")
-
-
-def Prosthetic_Rehabilitation(request):
-    return render(request, "Prosthetic_Rehabilitation.html")
 
 
 @login_required
@@ -385,36 +367,4 @@ def Provider_TimedGo_Test(request):
 def Provider_6Min_Test(request):
     return render(request, "Provider_6Min_Test.html")
 
-"""
 
-Removed while updating to new HTML format 
- 
-def self_score(request, step):
-    if (step == "PS"):
-        return render(request, "Self_Score_Base.html", {"step": "Postsurgical Stabilization"})
-    if (step == "PPR"):
-        return render(request, "Self_Score_Base.html", {"step": "Preprosthetic Rehabilitation"})
-    if (step == "LHM"):
-        return render(request, "Self_Score_Base.html", {"step": "Limb Healing &  Maturation"})
-    if (step == "PF"):
-        return render(request, "Self_Score_Base.html", {"step": "Prosthetic Fitting"})
-    if (step == "PR"):
-        return render(request, "Self_Score_Base.html", {"step": "Prosthetic Rehabilitation"})
-
-
-def sensor_score(request):
-    return render(request, "Sensor_Score_Base.html")
-
-def sensor_graph(request,step):
-    if (step == "Cadence"):
-        return render(request, "Sensor_Graph_Base.html", {"type": "Cadence"})
-    if (step == "WalkTime"):
-        return render(request, "Sensor_Graph_Base.html", {"type": "Walking Time"})
-    if (step == "DistanceWalked"):
-        return render(request, "Sensor_Graph_Base.html", {"type": "Distance Walked"})
-    if (step == "WalkSpeed"):
-        return render(request, "Sensor_Graph_Base.html", {"type": "Walking Speed"})
-    if (step == "StepHeight"):
-        return render(request, "Sensor_Graph_Base.html", {"type": "Step Height"})
-
-"""
