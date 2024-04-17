794eda72a5a629a0de361348e16f8a5f86755fa4
Jagger <jaggergilleland@gmail.com>
2024-04-16 21:01:39 -0400

    Test to see if images work v5

--- LowerLimbLoss/settings.py
+++ LowerLimbLoss/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/LowerLimbLoss/Main_Aplication/static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
 
 
 # Default primary key field type
