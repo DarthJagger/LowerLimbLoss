4e6fbddac730000b78e7c972d6b3195723f9159c
Quindigon512 <128196417+Quindigon512@users.noreply.github.com>
2024-04-25 14:33:07 -0400

    Update forms.py

--- Main_Aplication/forms.py
+++ Main_Aplication/forms.py
@@ -19,3 +19,33 @@ class PatientEntryForm(forms.ModelForm):
         model = PatientEntries
         fields = ['phantom_limb_ps_avg', 'phantom_limb_ps_max', 'residual_limb_ps_avg', 'residual_limb_ps_max',
                   'socket_comfort_score_avg', 'socket_comfort_score_max']
+
+
+class ProviderAmpProForm(forms.ModelForm):
+    class Meta:
+        model = AmpproScores
+        fields = ['amppro', 'time_balanced_sound', 'time_balanced_prosthesis']
+
+
+class ProviderAmpNoProForm(forms.ModelForm):
+    class Meta:
+        model = AmpnoproScores
+        fields = ['ampnopro']
+
+
+class ProviderTimedGoForm(forms.ModelForm):
+    class Meta:
+        model = TimedupandgoScores
+        fields = ['timedupandgo']
+
+
+class Provider6MinForm(forms.ModelForm):
+    class Meta:
+        model = SixminwalktestScores
+        fields = ['sixminwalktest']
+
+
+class ProviderPlusMForm(forms.ModelForm):
+    class Meta:
+        model = PlusMScores
+        fields = ['plus_m']
